72991e91ed4ffc4c4e825a05f7e91373ad8916be
Andrew Zonenberg <azonenberg@drawersteak.com>
2024-02-26 13:17:54 -0800

    Added soft power-on support

--- firmware/super/main.cpp
+++ firmware/super/main.cpp
@@ -35,13 +35,13 @@ Logger g_log;
 
 Timer* g_logTimer;
 
+void InitGPIOs();
 void InitClocks();
 void InitUART();
 void InitLog();
 void DetectHardware();
 
 void StartRail(GPIOPin& en, GPIOPin& pgood, uint32_t timeout, const char* name);
-//void StartSubRail(GPIOPin& en, GPIOPin& pgood, uint32_t timeout, const char* name);
 void MonitorRail(GPIOPin& pgood, const char* name);
 void PanicShutdown();
 
@@ -54,16 +54,38 @@ GPIOPin* g_en_gtx_1v8 = nullptr;
 GPIOPin* g_en_3v3 = nullptr;
 GPIOPin* g_en_3v0_n = nullptr;
 
+GPIOPin* g_pgood_1v0 = nullptr;
+GPIOPin* g_pgood_1v2 = nullptr;
+GPIOPin* g_pgood_gtx_1v0 = nullptr;
+GPIOPin* g_pgood_gtx_1v8 = nullptr;
+GPIOPin* g_pgood_3v3 = nullptr;
+
 GPIOPin* g_fail_led = nullptr;
 GPIOPin* g_ok_led = nullptr;
-/*
 
 bool g_fpgaUp = false;
 GPIOPin* g_fpga_done = nullptr;
 GPIOPin* g_mcu_rst_n = nullptr;
+GPIOPin* g_fpga_rst_n = nullptr;
+
+GPIOPin* g_softPower = nullptr;
+GPIOPin* g_softReset = nullptr;
 
 void PollFPGA();
-*/
+
+void PollPowerButtons();
+
+enum
+{
+	STATE_OFF,			//nothing happening
+	STATE_STARTING,		//startup but button may still be pressed
+	STATE_ON,			//on, button released
+	STATE_STOPPING,		//shutdown but button may still be pressed
+} g_powerState = STATE_OFF;
+
+void PowerOn();
+void PowerOff();
+
 int main()
 {
 	//Copy .data from flash to SRAM (for some reason the default newlib startup won't do this??)
@@ -77,31 +99,161 @@ int main()
 	InitUART();
 	InitLog();
 	DetectHardware();
+	InitGPIOs();
+
+	//Wait 5 seconds in case something goes wrong during first power up
+	g_log("5 second delay\n");
+	g_logTimer->Sleep(50000);
+	g_log("Ready\n");
+
+	//Run the normal power-on procedure
+	//PowerOn();
+
+	//Main event loop
+	//TODO: support warm reset and/or hard power cycle on request
+	while(1)
+	{
+		//Check for overflows on our log message timer
+		g_log.UpdateOffset(60000);
+
+		//Check for power button activity
+		PollPowerButtons();
+
+		//Poll inputs and check to see if anything ever went out of spec
+		if(g_powerState == STATE_ON)
+		{
+			PollFPGA();
+
+			MonitorRail(*g_pgood_3v3, "3V3");
+			//MonitorRail(pgood_1v8, "1V8");
+			MonitorRail(*g_pgood_gtx_1v8, "GTX_1V8");
+			MonitorRail(*g_pgood_1v2, "1V2");
+			MonitorRail(*g_pgood_1v0, "1V0");
+			MonitorRail(*g_pgood_gtx_1v0, "GTX_1V0");
+		}
+	}
+
+	return 0;
+}
+
+/**
+	@brief Turn off all power rails and reset things cleanly
+ */
+void PowerOff()
+{
+	g_log("Beginning power-down sequence\n");
+	LogIndenter li(g_log);
+
+	*g_en_3v0_n = 0;
+	*g_en_3v3 = 0;
+	*g_en_1v2 = 0;
+	*g_en_1v8 = 0;
+	*g_en_gtx_1v8 = 0;
+	*g_en_1v0 = 0;
+	*g_en_gtx_1v0 = 0;
+	*g_en_12v0 = 0;
+
+	*g_fail_led = 0;
+	*g_ok_led = 0;
+
+	g_powerState = STATE_STOPPING;
+}
+
+/**
+	@brief Turn on all power rails and reset things cleanly
+ */
+void PowerOn()
+{
+	g_log("Beginning power-up sequence\n");
+	LogIndenter li(g_log);
+
+	//Hold everything in reset during powerup
+	*g_fpga_rst_n = 0;
+	*g_mcu_rst_n = 0;
+
+	//Before we can bring up anything else, the 12V rail has to come up
+	//12V ramp rate is slew rate controlled to about 2 kV/sec, so should take 0.5 ms to come up
+	//Give it 5 ms to be safe (plus extra delay from UART messages)
+	//(we don't have any sensing on this rail so we have to just hope it came up)
+	g_log("Turning on 12V0\n");
+	*g_en_12v0 = 1;
+	g_logTimer->Sleep(50);
+
+	//TODO: poll the 12V rail via our ADC and measure it to verify it's good
+
+	//Now turn on the core power DC-DC's, giving them each 5ms to come up
+	StartRail(*g_en_1v0, *g_pgood_1v0, 50, "1V0");
+	StartRail(*g_en_gtx_1v0, *g_pgood_1v0, 50, "GTX_1V0");
+
+	//1V8 runs VCCAUX and should come up before VCCO
+	//Turn on 1V8 and hope for the best since PGOOD isn't working (probable short)
+	g_log("Turning on 1V8, ignoring PGOOD\n");
+	*g_en_1v8 = 1;
+	g_logTimer->Sleep(50);
+	//StartRail(en_1v8, pgood_1v8, 50, "1V8");
+
+	//No recommended sequence for GTX_1V8, now is as good a time as any to turn it on
+	StartRail(*g_en_gtx_1v8, *g_pgood_gtx_1v8, 50, "GTX_1V8");
+
+	//3V3 needs to come up after 1V8 so that VCCO - VCCAUX (1V8) is always <2.625V
+	StartRail(*g_en_3v3, *g_pgood_3v3, 50, "3V3");
+
+	//1V2 should turn on at the same time or later than GTX_1V0
+	StartRail(*g_en_1v2, *g_pgood_1v2, 50, "1V2");
+
+	//Turn on 3V0_N last
+	//g_log("Turning on 3V0_N (no PGOOD signal available)\n");
+	//en_3v0_n = 1;
+
+	//All power rails came up if we get here
+	g_log("Power is good, releasing FPGA reset\n");
+	*g_ok_led = 1;
+	*g_fpga_rst_n = 1;
+
+	//Everything started up if we get here
+	//TODO: wait for heartbeat from MCU too?
+	if(*g_softPower)
+		g_powerState = STATE_STARTING;
+	else
+		g_powerState = STATE_ON;
+}
+
+void InitGPIOs()
+{
+	g_log("Initializing GPIOs\n");
 
 	//Initalize our GPIOs and make sure all rails are off
-	GPIOPin en_12v0(&GPIOB, 5, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
-	GPIOPin en_1v0(&GPIOB, 9, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
-	GPIOPin en_gtx_1v0(&GPIOB, 10, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
-	GPIOPin en_1v2(&GPIOC, 13, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
-	GPIOPin en_1v8(&GPIOB, 14, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
-	GPIOPin en_gtx_1v8(&GPIOB, 2, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
-	GPIOPin en_3v3(&GPIOB, 4, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
-	GPIOPin en_3v0_n(&GPIOC, 13, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
-	GPIOPin pgood_1v0(&GPIOB, 8, GPIOPin::MODE_INPUT, 0, false);
-	GPIOPin pgood_gtx_1v0(&GPIOA, 2, GPIOPin::MODE_INPUT, 0, false);
-	GPIOPin pgood_1v2(&GPIOC, 0, GPIOPin::MODE_INPUT, 0, false);
-	GPIOPin pgood_1v8(&GPIOB, 13, GPIOPin::MODE_INPUT, 0, false);
-	GPIOPin pgood_gtx_1v8(&GPIOB, 0, GPIOPin::MODE_INPUT, 0, false);
-	GPIOPin pgood_3v3(&GPIOB, 3, GPIOPin::MODE_INPUT, 0, false);
+	static GPIOPin en_12v0(&GPIOB, 5, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
+	static GPIOPin en_1v0(&GPIOB, 9, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
+	static GPIOPin en_gtx_1v0(&GPIOB, 10, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
+	static GPIOPin en_1v2(&GPIOC, 13, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
+	static GPIOPin en_1v8(&GPIOB, 14, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
+	static GPIOPin en_gtx_1v8(&GPIOB, 2, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
+	static GPIOPin en_3v3(&GPIOB, 4, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
+	static GPIOPin en_3v0_n(&GPIOC, 13, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
 
-	GPIOPin fail_led(&GPIOH, 1, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
-	GPIOPin ok_led(&GPIOH, 0, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
-	/*
+	static GPIOPin pgood_1v0(&GPIOB, 8, GPIOPin::MODE_INPUT, 0, false);
+	static GPIOPin pgood_gtx_1v0(&GPIOA, 2, GPIOPin::MODE_INPUT, 0, false);
+	static GPIOPin pgood_1v2(&GPIOC, 0, GPIOPin::MODE_INPUT, 0, false);
+	static GPIOPin pgood_1v8(&GPIOB, 13, GPIOPin::MODE_INPUT, 0, false);
+	static GPIOPin pgood_gtx_1v8(&GPIOB, 0, GPIOPin::MODE_INPUT, 0, false);
+	static GPIOPin pgood_3v3(&GPIOB, 3, GPIOPin::MODE_INPUT, 0, false);
 
-	GPIOPin fpga_done(&GPIOB, 2, GPIOPin::MODE_INPUT, 0, false);
-	GPIOPin mcu_rst_n(&GPIOB, 0, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
-	GPIOPin fpga_rst_n(&GPIOA, 3, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW, 0, true);
-	*/
+	static GPIOPin fail_led(&GPIOH, 1, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
+	static GPIOPin ok_led(&GPIOH, 0, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
+
+	static GPIOPin fpga_done(&GPIOB, 11, GPIOPin::MODE_INPUT, 0, false);
+	static GPIOPin mcu_rst_n(&GPIOA, 3, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW);
+	static GPIOPin mcu_ready(&GPIOA, 0, GPIOPin::MODE_INPUT, 0, false);
+	static GPIOPin fpga_rst_n(&GPIOA, 1, GPIOPin::MODE_OUTPUT, GPIOPin::SLEW_SLOW, 0, true);
+
+	static GPIOPin pwr_button(&GPIOA, 12, GPIOPin::MODE_INPUT, 0, false);
+	static GPIOPin rst_button(&GPIOA, 15, GPIOPin::MODE_INPUT, 0, false);
+
+	//R28 got knocked off the prototype during rework and I didn't feel like putting it back on
+	//On die pullups FTW
+	pgood_1v2.SetPullMode(GPIOPin::PULL_UP);
+
 	en_12v0 = 0;
 	en_1v0 = 0;
 	en_gtx_1v0 = 0;
@@ -109,9 +261,8 @@ int main()
 	en_1v8 = 0;
 	en_gtx_1v8 = 0;
 
-	/*mcu_rst_n = 0;
+	mcu_rst_n = 0;
 	fpga_rst_n = 0;
-	*/
 	fail_led = 0;
 	ok_led = 0;
 
@@ -125,119 +276,94 @@ int main()
 	g_en_3v3 = &en_3v3;
 	g_en_3v0_n = &en_3v0_n;
 
+	g_pgood_1v0 = &pgood_1v0;
+	g_pgood_gtx_1v0 = &pgood_gtx_1v0;
+	g_pgood_gtx_1v8 = &pgood_gtx_1v8;
+	g_pgood_1v2 = &pgood_1v2;
+	g_pgood_3v3 = &pgood_3v3;
+
 	g_fail_led = &fail_led;
 	g_ok_led = &ok_led;
-	/*
+
 	g_fpga_done = &fpga_done;
 	g_mcu_rst_n = &mcu_rst_n;
-	*/
-	//Wait 5 seconds in case something goes wrong during first power up
-	g_log("5 second delay\n");
-	g_logTimer->Sleep(50000);
+	g_fpga_rst_n = &fpga_rst_n;
 
-	//Before we can bring up anything else, the 12V rail has to come up
-	//12V ramp rate is slew rate controlled to about 2 kV/sec, so should take 0.5 ms to come up
-	//Give it 5 ms to be safe (plus extra delay from UART messages)
-	//(we don't have any sensing on this rail so we have to just hope it came up)
-	g_log("Turning on 12V0\n");
-	en_12v0 = 1;
-	g_logTimer->Sleep(50);
-
-	//TODO: poll the 12V rail via our ADC and measure it to verify it's good
-
-	//Now turn on the core power DC-DC's, giving them each 5ms to come up
-	ok_led = 1;
-	StartRail(en_1v0, pgood_1v0, 50, "1V0");
-	//StartRail(en_gtx_1v0, pgood_1v0, 50, "GTX_1V0");
-
-	//1V8 runs VCCAUX and should come up before VCCO
-	//Turn on 1V8 and hope for the best
-	g_log("Turning on 1V8, ignoring PGOOD\n");
-	en_1v8 = 1;
-	g_logTimer->Sleep(50);
-	//StartRail(en_1v8, pgood_1v8, 50, "1V8");
-
-	//Then GTX 1V8
-	//StartRail(en_gtx_1v8, pgood_gtx_1v8, 50, "GTX_1V8");
-
-	//Try to bring up 3V3 next
-	StartRail(en_3v3, pgood_3v3, 50, "3V3");
-
-	//TODO where should we sequence 1V2
-	//StartRail(en_1v2, pgood_1v2, 50, "1V2");
-
-	//Turn on 3V0_N last
-	//g_log("Turning on 3V0_N (no PGOOD signal available)\n");
-	//en_3v0_n = 1;
-
-	/*
-
-	//We really SHOULD start 1V8 then 3V3, but then U18 will have PVIN > AVIN and bad things will happen
-	//to Vref / Vtt. This will be fixed in LATENTRED by running the LP2996 off 3V3_SB instead of 3V3.
-
-	//DP83867 requires 2.5 analog up before 1.8 analog (no requirement for analog vs vccio rail)
-	StartRail(en_2v5, pgood_2v5, 50, "2V5");
-
-	//We then need to start 1V8 right away (within Tvcco2vccaux of starting 3V3) to avoid damage to the FPGA!
-	//1.8V must be stable within 25ms of 2.5V ramping up
-	StartRail(en_1v8, pgood_1v8, 50, "1V8");
-
-	//1V8_IO and Vref/Vtt have no PGOOD feedback.
-	//If it overcurrents, they will probably drag down the main 1V8 rail so check for faults over the next 10ms
-	StartSubRail(en_1v8_io, pgood_1v8, 100, "1V8_IO");
-	StartSubRail(en_vtt, pgood_1v8, 100, "Vtt + Vref");
-
-	//All power rails came up if we get here
-	g_log("Power is good, releasing FPGA reset\n");
-	pwr_ok_led = 1;
-	fpga_rst_n = 1;
-
-	//Everything started up if we get here
-	//TODO: wait for heartbeat from MCU too
-
-	//Poll inputs and check to see if anything ever went out of spec
-	//TODO: support warm reset and/or hard power cycle on request
-	*/
-	while(1)
-	{
-		//Check for overflows on our log message timer
-		g_log.UpdateOffset(60000);
-
-		//PollFPGA();
-
-		MonitorRail(pgood_3v3, "3V3");
-		//MonitorRail(pgood_2v5, "2V5");
-		//MonitorRail(pgood_1v8, "1V8");
-		//MonitorRail(pgood_gtx_1v8, "GTX_1V8");
-		//MonitorRail(pgood_1v2, "1V2");
-		MonitorRail(pgood_1v0, "1V0");
-		//MonitorRail(pgood_gtx_1v0, "GTX_1V0");
-	}
-
-	return 0;
+	g_softPower = &pwr_button;
+	g_softReset = &rst_button;
 }
 
-/*
+/**
+	@brief Check for soft power button presses
+ */
+void PollPowerButtons()
+{
+	//Power button state machine
+	switch(g_powerState)
+	{
+		//Off to starting
+		case STATE_OFF:
+			if(*g_softPower)
+			{
+				g_log("Soft power-up requested\n");
+				PowerOn();
+			}
+			break;
+
+		//Starting up but button is possibly still pressed
+		case STATE_STARTING:
+			if(!*g_softPower)
+			{
+				g_log("Power button released\n");
+				g_powerState = STATE_ON;
+			}
+			break;
+
+		//Shutting down
+		case STATE_ON:
+			if(*g_softPower)
+			{
+				g_log("Soft power-down requested\n");
+				PowerOff();
+			}
+			break;
+
+		//Shutting down but button is possibly still pressed
+		case STATE_STOPPING:
+			if(!*g_softPower)
+			{
+				g_log("Power button released\n");
+				g_powerState = STATE_OFF;
+			}
+			break;
+	}
+
+	//Reset button state machine
+}
+
+/**
+	@brief Check for DONE pin state changes
+ */
 void PollFPGA()
 {
-	bool done = g_fpga_done->Get();
+	bool done = *g_fpga_done;
 
 	if(done && !g_fpgaUp)
 	{
-		g_all_ok_led->Set(true);
+		*g_ok_led = 1;
 		g_log("FPGA is up, releasing MCU reset\n");
-		g_mcu_rst_n->Set(true);
+		*g_mcu_rst_n = 1;
 		g_fpgaUp = true;
 	}
 
 	else if(!done && g_fpgaUp)
 	{
-		g_all_ok_led->Set(false);
+		*g_ok_led = 0;
 		g_log("FPGA is down, resetting MCU\n");
-		g_mcu_rst_n->Set(false);
+		*g_mcu_rst_n = 0;
 		g_fpgaUp = false;
 	}
-}*/
+}
 
 /**
 	@brief Polls a power rail and look for signs of trouble
@@ -259,17 +385,19 @@ void MonitorRail(GPIOPin& pgood, const char* name)
  */
 void PanicShutdown()
 {
-	g_en_3v0_n->Set(false);
-	g_en_3v3->Set(false);
-	g_en_1v2->Set(false);
-	g_en_1v8->Set(false);
-	g_en_gtx_1v8->Set(false);
-	g_en_1v0->Set(false);
-	g_en_gtx_1v0->Set(false);
-	g_en_12v0->Set(false);
+	*g_en_3v0_n = 0;
+	*g_en_3v3 = 0;
+	*g_en_1v2 = 0;
+	*g_en_1v8 = 0;
+	*g_en_gtx_1v8 = 0;
+	*g_en_1v0 = 0;
+	*g_en_gtx_1v0 = 0;
+	*g_en_12v0 = 0;
 
-	g_fail_led->Set(1);
-	g_ok_led->Set(0);
+	*g_fail_led = 1;
+	*g_ok_led = 0;
+
+	g_powerState = STATE_OFF;
 }
 
 /**
@@ -297,31 +425,6 @@ void StartRail(GPIOPin& en, GPIOPin& pgood, uint32_t timeout, const char* name)
 	}
 }
 
-/**
-	@brief Turns on a sub-rail switched or regulated from a parent rail, but lacking its own PGOOD output
- */
- /*
-void StartSubRail(GPIOPin& en, GPIOPin& pgood, uint32_t timeout, const char* name)
-{
-	//These rails have no PGOOD feedback.
-	//If it overcurrents, it will probably drag down the parent rail so check for faults over a fixed time window
-	g_log("Turning on %s\n", name);
-	en = 1;
-	for(uint32_t i=0; i<timeout; i++)
-	{
-		if(!pgood)
-		{
-			PanicShutdown();
-
-			g_log(Logger::ERROR, "Rail %s failed to come up\n", name);
-
-			while(1)
-			{}
-		}
-		g_logTimer->Sleep(1);
-	}
-}
-*/
 void InitClocks()
 {
 	//No need to mess with flash wait states
